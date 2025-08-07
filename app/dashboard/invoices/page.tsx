e2207a93fc7fd43061b4e8b99fc3be27944a5576
Shresth-001 <shresth.hp@gmail.com>
2025-08-07 17:21:43 +0530

    Added pagination following with nextjs doc

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -5,6 +5,7 @@ import InvoicesTable from "@/app/ui/invoices/table";
 import Search from "@/app/ui/search";
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import { Suspense } from "react";
+import { fetchInvoicesPages } from "@/app/lib/data";
 
 export default async function Page(props:{searchParams?:Promise<{
     query?:string;
@@ -14,6 +15,7 @@ export default async function Page(props:{searchParams?:Promise<{
     const searchParams=await props.searchParams;
     const query=searchParams?.query||'';
     const currentPage=Number(searchParams?.page)||1;
+    const totalPages=await fetchInvoicesPages(query);
     return (
         <div className="w-full">
             <div className="flex w-full items-center justify-between">
@@ -27,7 +29,7 @@ export default async function Page(props:{searchParams?:Promise<{
                 <InvoicesTable query={query} currentPage={currentPage}/>
             </Suspense>
             <div className="mt-5 flex w-full justify-center">
-                {/* <Pagination totalPages={totalPages}/> */}
+                <Pagination totalPages={totalPages}/>
             </div>
             
         </div>
